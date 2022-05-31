09b0ac853dc200f7196cf9913750e98535204f39
Gustavo0033 <gu2003.mendonca48@gmail.com>
2022-05-31 16:34:08 -0300

    Team Liquid

--- app/src/main/java/com/example/teamliquidproject/MainActivity.java
+++ app/src/main/java/com/example/teamliquidproject/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.teamliquidproject;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -24,38 +25,49 @@ public class MainActivity extends AppCompatActivity {
     private View btn_signIN;
     private View btn_signUP;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Liquidmain = findViewById(R.id.main_Liquid);
 
         List<Mainitem> mainItems = new ArrayList<>();
         mainItems.add(new Mainitem(1, R.drawable._00px_team_liquid_2020_full_lightmode, R.string.app_name, Color.BLUE));
         mainItems.add(new Mainitem(1, R.drawable._00px_team_liquid_2020_full_lightmode, R.string.app_name, Color.GREEN));
-        mainItems.add(new Mainitem(1, R.drawable._00px_team_liquid_2020_full_lightmode, R.string.app_name, Color.RED));
-        mainItems.add(new Mainitem(1, R.drawable.og_logo, R.string.app_name, Color.GRAY));
-        mainItems.add(new Mainitem(1, R.drawable.og_logo, R.string.app_name, Color.MAGENTA));
 
 
 
-        Liquidmain = findViewById(R.id.main_Liquid);
-        Liquidmain.setLayoutManager(new LinearLayoutManager(this));
+        Liquidmain.setLayoutManager(new GridLayoutManager(this, 2));
         MainAdapter adapter = new MainAdapter(mainItems);
         Liquidmain.setAdapter(adapter);
 
 
+        adapter.setListener(id ->{
+            switch (id) {
+                case 1:
+                    startActivity(new Intent(MainActivity.this, LiquidActivity.class));
+                    break;
+                case 2:
+                    startActivity(new Intent(MainActivity.this, signUP.class));
+                    break;
+            }
+        });
 
-
-
+        Liquidmain.setAdapter(adapter);
 
 
     }
-    private class MainAdapter extends RecyclerView.Adapter<MainViewHolder>{
+    private class MainAdapter extends RecyclerView.Adapter<MainAdapter.MainViewHolder>{
 
        private List<Mainitem> mainItems;
+       private OnitemClickListener Listener;
 
         public MainAdapter(List<Mainitem> mainitems) {
             this.mainItems = mainitems;
+        }
+        public void setListener(OnitemClickListener listener) {
+            this.Listener = listener;
         }
 
         @NonNull
@@ -74,23 +86,31 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return mainItems.size() ;
         }
-    }
+        private class MainViewHolder extends RecyclerView.ViewHolder{
 
-    private class MainViewHolder extends RecyclerView.ViewHolder{
+            public MainViewHolder(@NonNull View itemView) {
+                super(itemView);
+            }
+            public void bind( Mainitem item) {
+                TextView txtName = itemView.findViewById(R.id.txt_loja);
+                ImageView imgICon = itemView.findViewById(R.id.Liquid_logo);
+                LinearLayout container = (LinearLayout) itemView;
 
-        public MainViewHolder(@NonNull View itemView) {
-            super(itemView);
+                container.setOnClickListener(view ->{
+                        Listener.onClick(item.getId());
+
+                        Liquidmain.setOnClickListener(View ->{
+                        Listener.onClick(item.getId());
+                        });
+            });
+
+                txtName.setText(item.getTextStringId());
+                imgICon.setImageResource(item.getDrawbleid());
+                container.setBackgroundColor(item.getColor());
+            }
+
         }
-        public void bind( Mainitem item) {
-            TextView txtName = itemView.findViewById(R.id.txt_loja);
-            ImageView imgICon = itemView.findViewById(R.id.Liquid_logo);
-            LinearLayout container = (LinearLayout) itemView;
 
-            txtName.setText(item.getTextStringId());
-            imgICon.setImageResource(item.getDrawbleid());
-            container.setBackgroundColor(item.getColor());
-        }
+
     }
-
-
-}
+    }
---
+++ app/src/main/java/com/example/teamliquidproject/OnitemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.teamliquidproject;
+
+import android.view.View;
+
+public interface OnitemClickListener {
+    void onClick(int Id);
+}
